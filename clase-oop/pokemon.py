605beacc5a06ae97b1ebb81ab2a8258544b0f3a6
Mario Pesca <ing.mpesca@gmail.com>
2023-10-31 14:16:48 -0500

    add interactions

--- clase-oop/pokemon.py
+++ clase-oop/pokemon.py
@@ -1,11 +1,15 @@
 class Pokemon:
-    def __init__(self, nombre, tipo, salud=100):
+    def __init__(self, nombre, tipo, salud=100, ataque=10, defensa=5, experiencia=0):
         self.nombre = nombre
         self.tipo = tipo
         self.salud = salud
+        self.ataque = ataque
+        self.defensa = defensa
+        self.experiencia = experiencia
 
     def atacar(self, otro_pokemon):
-        danio = 10
+        danio = self.ataque - otro_pokemon.defensa
+        danio = max(danio, 0)
         print(f"{self.nombre} ataca a {otro_pokemon.nombre} y le causa {danio} de daño!")
         otro_pokemon.recibir_danio(danio)
 
@@ -23,6 +27,7 @@ class Pokedex:
 
     def agregar_pokemon(self, pokemon):
         self.pokemon_lista.append(pokemon)
+        print(f"{pokemon.nombre} ha sido agregado a la Pokedex.")
 
     def eliminar_pokemon(self, nombre):
         for pokemon in self.pokemon_lista:
@@ -35,7 +40,7 @@ class Pokedex:
     def mostrar_pokemon(self):
         print("Pokémon en la Pokedex:")
         for pokemon in self.pokemon_lista:
-            print(f"Nombre: {pokemon.nombre}, Tipo: {pokemon.tipo}, Salud: {pokemon.salud}")
+            print(f"Nombre: {pokemon.nombre}, Tipo: {pokemon.tipo}, Salud: {pokemon.salud}, Ataque: {pokemon.ataque}, Defensa: {pokemon.defensa}, Experiencia: {pokemon.experiencia}")
 
     def batalla(self, nombre1, nombre2):
         pokemon1 = None
@@ -56,32 +61,88 @@ class Pokedex:
             pokemon1.atacar(pokemon2)
             if pokemon2.salud <= 0:
                 print(f"{pokemon1.nombre} ganó la batalla!")
+                pokemon1.experiencia += 10
+                print(f"{pokemon1.nombre} ganó 10 puntos de experiencia.")
                 break
             pokemon2.atacar(pokemon1)
             if pokemon1.salud <= 0:
                 print(f"{pokemon2.nombre} ganó la batalla!")
+                pokemon2.experiencia += 10
+                print(f"{pokemon2.nombre} ganó 10 puntos de experiencia.")
 
 
 # Crear una Pokedex
 pokedex = Pokedex()
 
-# Agregar Pokémon
-pikachu = Pokemon("Pikachu", "Eléctrico", 100)
-charmander = Pokemon("Charmander", "Fuego", 100)
-bulbasaur = Pokemon("Bulbasaur", "Planta", 100)
+while True:
+    print("\nOpciones:")
+    print("1. Agregar Pokémon")
+    print("2. Mostrar Pokémon")
+    print("3. Realizar Batalla")
+    print("4. Salir")
+    opcion = input("Selecciona una opción: ")
 
-pokedex.agregar_pokemon(pikachu)
-pokedex.agregar_pokemon(charmander)
-pokedex.agregar_pokemon(bulbasaur)
+    if opcion == "1":
+        nombre = input("Nombre del Pokémon: ")
+        tipo = input("Tipo del Pokémon: ")
+        salud = int(input("Salud del Pokémon: "))
+        ataque = int(input("Ataque del Pokémon: "))
+        defensa = int(input("Defensa del Pokémon: "))
+        experiencia = int(input("Experiencia del Pokémon: "))
+        nuevo_pokemon = Pokemon(nombre, tipo, salud, ataque, defensa, experiencia)
+        pokedex.agregar_pokemon(nuevo_pokemon)
 
-# Mostrar Pokémon en la Pokedex
-pokedex.mostrar_pokemon()
+    elif opcion == "2":
+        pokedex.mostrar_pokemon()
 
-# Realizar una batalla
-pokedex.batalla("Pikachu", "Charmander")
+    elif opcion == "3":
+        nombre1 = input("Nombre del primer Pokémon en la batalla: ")
+        nombre2 = input("Nombre del segundo Pokémon en la batalla: ")
+        pokedex.batalla(nombre1, nombre2)
 
-# Eliminar un Pokémon
-pokedex.eliminar_pokemon("Bulbasaur")
+    elif opcion == "4":
+        print("Saliendo del programa.")
+        break
 
-# Mostrar Pokémon actualizados en la Pokedex
-pokedex.mostrar_pokemon()
+    else:
+        print("Opción no válida. Por favor, selecciona una opción válida.")
+
+
+
+# Crear una Pokedex
+pokedex = Pokedex()
+
+while True:
+    print("\nOpciones:")
+    print("1. Agregar Pokémon")
+    print("2. Mostrar Pokémon")
+    print("3. Realizar Batalla")
+    print("4. Salir")
+    opcion = input("Selecciona una opción: ")
+
+    if opcion == "1":
+        nombre = input("Nombre del Pokémon: ")
+        tipo = input("Tipo del Pokémon: ")
+        salud = int(input("Salud del Pokémon: "))
+        ataque = int(input("Ataque del Pokémon: "))
+        defensa = int(input("Defensa del Pokémon: "))
+        experiencia = int(input("Experiencia del Pokémon: "))
+        nuevo_pokemon = Pokemon(nombre, tipo, salud, ataque, defensa, experiencia)
+        pokedex.agregar_pokemon(nuevo_pokemon)
+        print(f"{nombre} ha sido agregado a la Pokedex.")
+
+    elif opcion == "2":
+        pokedex.mostrar_pokemon()
+
+    elif opcion == "3":
+        nombre1 = input("Nombre del primer Pokémon en la batalla: ")
+        nombre2 = input("Nombre del segundo Pokémon en la batalla: ")
+        pokedex.batalla(nombre1, nombre2)
+
+    elif opcion == "4":
+        print("Saliendo del programa.")
+        break
+
+    else:
+        print("Opción no válida. Por favor, selecciona una opción válida.")
+
